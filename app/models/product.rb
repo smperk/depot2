ca0871cda6e4b4355e4a62ca3f8e09d26f4a5ccc
Sean Maurice Perkins <sean.perkins@rcn.net>
2014-01-23 11:41:42 -0500

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,7 @@ validates :title, :description, :image_url, presence: true
 validates :price, numericality: {greater_than_or_equal_to: 0.01}
 validates :title, uniqueness: true
 validates :image_url, allow_blank: true, format: {
-   with: %r{\.(gif|jpg|png)\Z}i,
-   message: 'must be a URL for GIF, JPG or PNG image.'
+   with:            %r{\.(gif|jpg|png)\Z}i,
+   message:         'must be a URL for GIF, JPG or PNG image.'
 }
-end
-
-def self.latest
-  Product.order(:updated_at).last
 end
